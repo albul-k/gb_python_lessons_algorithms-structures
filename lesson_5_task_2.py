c55964dc0a9cfdabade8778050aa13a6620b7ee8
@albul-k <konstantin.albul@gmail.com>
2020-06-06 16:33:00 +0300

    task2 done

--- lesson_5_task_2.py
+++ lesson_5_task_2.py
@@ -4,37 +4,49 @@
 
 from collections import deque
 
-HEX_TO_DEC = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7,
-              8: 8, 9: 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
-DEC_TO_HEX = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7,
-              8: 8, 9: 9, 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+HEX_TO_DEC = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+DEC_TO_HEX = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+
+def hex_to_dec(num: str) -> int:
+    num_dec = 0
+    for ind, value in enumerate(num[::-1]):
+        if value.isdigit():
+            num = int(value)
+        else:
+            num = HEX_TO_DEC[value.capitalize()]
+        num_dec += num * (16 ** ind)
+    return num_dec
+
+
+def dec_to_hex(num: str) -> list:
+    res = deque()
+    while num > 0:
+        _t = num % 16
+        if _t >= 10:
+            _t = DEC_TO_HEX[_t]
+        res.appendleft(str(_t))
+        num = num // 16
+    return list(res)
 
 
 def hex_sum(a: str, b: str) -> str:
-    res = []
-    hex_1, hex_2 = deque(a), deque(b)
-    # if len(hex_1) > len(hex_2):
-    #     while len(hex_1):
-    #         _sum = hex_1[-1] + hex_2[-1]
-    #         if _sum > 9
-    #     for itm in hex_1[::-1]:
-
-    # else:
-    #     pass
-
-    return ('').join(res)
+    return dec_to_hex(hex_to_dec(a) + hex_to_dec(b))
 
 
 def hex_mul(a: str, b: str) -> str:
-    pass
+    return dec_to_hex(hex_to_dec(a) * hex_to_dec(b))
 
 
 inp_1 = input('Введите первое шестнадцатеричное число: ')
 inp_2 = input('Введите второе шестнадцатеричное число: ')
 
-print(hex_sum(inp_1, inp_2))
+print(f'Результат сложения: {hex_sum(inp_1, inp_2)}')
+print(f'Проверка: {hex(int(inp_1, 16) + int(inp_2, 16))}')
 
-# print(hex(int(inp_1, 16) + int(inp_2, 16)))
+print(f'\nРезультат умножения: {hex_mul(inp_1, inp_2)}')
+print(f'Проверка: {hex(int(inp_1, 16) * int(inp_2, 16))}')
 
-# if __name__ == "__main__":
-#     assert(hex_sum('A2', 'C4F')) ==  ['C', 'F', '1']
+if __name__ == "__main__":
+    assert(hex_sum('A2', 'C4F')) == ['C', 'F', '1']
+    assert(hex_mul('A2', 'C4F')) == ['7', 'C', '9', 'F', 'E']
